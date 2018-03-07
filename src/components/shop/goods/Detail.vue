68462b394dfbfd8f16372d364ca6db8f5ed55206
lijianpeng <1060397920@qq.com>
2018-03-07 15:42:07 +0800

    jquery插件放大镜效果

--- src/components/shop/goods/Detail.vue
+++ src/components/shop/goods/Detail.vue
@@ -1,5 +1,5 @@
 <template>
-     <div>
+    <div>
         <!-- 导航栏 -->
         <div class="section">
             <div class="location">
@@ -17,11 +17,37 @@
                     <!--页面左边-->
                     <div class="left-925">
                         <div class="goods-box clearfix">
-                            <!--商品图片-->
+                            <!-- 商品放大镜 -->
                             <div class="pic-box">
+                                <div class="magnifier" id="magnifier1">
+                                    <div class="magnifier-container">
+                                        <div class="images-cover"></div>
+                                        <!--当前图片显示容器-->
+                                        <div class="move-view"></div>
+                                        <!--跟随鼠标移动的盒子-->
+                                    </div>
+                                    <div class="magnifier-assembly">
+                                        <div class="magnifier-btn">
+                                            <span class="magnifier-btn-left">&lt;</span>
+                                            <span class="magnifier-btn-right">&gt;</span>
+                                        </div>
+                                        <!--按钮组-->
+                                        <div class="magnifier-line">
+                                            <ul class="clearfix animation03">
+                                                <li v-for="item in top.imglist" :key="item.id">
+                                                    <div class="small-img">
+                                                        <img :src="item.original_path" />
+                                                    </div>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                        <!--缩略图-->
+                                    </div>
+                                    <div class="magnifier-view"></div>
+                                    <!--经过放大的图片显示容器-->
+                                </div>
 
                             </div>
-                            <!--/商品图片-->
 
                             <!--商品信息-->
                             <div class="goods-spec">
@@ -106,57 +132,78 @@
                 </div>
             </div>
         </div>
-     </div>
- </template>
+    </div>
+</template>
  
  <script>
-    import AppAside from './subcom/CommonAside.vue';
+// 导入放大镜插件
+import "@/lib/imgzoom/css/magnifier.css";
+import "@/lib/imgzoom/js/magnifier.js";
+import $ from "jquery";
+import AppAside from "./subcom/CommonAside.vue";
 
-     export default {
+export default {
+  components: {
+    AppAside
+  },
 
-        components: {
-            AppAside
-        },
-
-        data() {
-            return {
-                id: this.$route.params.id,
-                num: 1,
-                top: {
-                    goodsinfo: {},
-                    imglist: [],
-                    hotgoodslist: []
-                }
-            }
-        },
-
-        methods: {
-            // 根据当前id获取数据
-            getTop() {
-                this.$http.get(this.$api.goodsDetail + this.id).then(res => {
-                    if(res.data.status == 0) {
-                        this.top = res.data.message;
-                    }
-                });
-            }
-        },
-
-        created() {
-            this.getTop();
-        },
-
-        // 商品详情页面的右侧列表, 可以点击切换不同的商品进行预览
-        // 但是默认情况下当前页面切换到当前页面不会触发组件的重新渲染, 为了解决这个问题,
-        // 我们可以监听$route对象的变化, 因为切换商品后, $route.params.id变化了, 我们监听它, 
-        // 然后主动发起http请求, 调用接口获取新id的数据进行视图刷新
-        watch: {
-            $route() {
-                this.id = this.$route.params.id;
-                this.getTop();
-            }
-        }
+  data() {
+    return {
+      id: this.$route.params.id,
+      num: 1,
+      top: {
+        goodsinfo: {},
+        imglist: [],
+        hotgoodslist: []
+      }
     };
- </script>
+  },
+
+  methods: {
+    // 根据当前id获取数据
+    getTop() {
+      this.$http.get(this.$api.goodsDetail + this.id).then(res => {
+        if (res.data.status == 0) {
+          this.top = res.data.message;
+        }
+      });
+    }
+  },
+
+  // 组件初始化完毕, 数据可用时被调用
+  created() {
+    this.getTop();
+  },
+
+        // 视图挂载到页面上了, 这里可操作DOM
+        mounted() {
+            var magnifierConfig = {
+                magnifier : "#magnifier1",//最外层的大容器
+                width : 370,//承载容器宽
+                height : 370,//承载容器高
+                moveWidth : null,//如果设置了移动盒子的宽度，则不计算缩放比例
+                zoom : 5//缩放比例
+            };
+
+            // 调用这个插件的方法, 必须保证放大镜相关的DOM节点都已经正常构建并渲染, 
+            // 但是我们的节点里面有个v-for动态生成列表, 调用该方法时不能保证列表已经构建完毕, 所以延时一下
+            setTimeout(function() {
+                var _magnifier = $().imgzoon(magnifierConfig);
+            }, 500);
+            
+        },
+  // 商品详情页面的右侧列表, 可以点击切换不同的商品进行预览
+  // 但是默认情况下当前页面切换到当前页面不会触发组件的重新渲染, 为了解决这个问题,
+  // 我们可以监听$route对象的变化, 因为切换商品后, $route.params.id变化了, 我们监听它,
+  // 然后主动发起http请求, 调用接口获取新id的数据进行视图刷新
+  watch: {
+    $route() {
+      this.id = this.$route.params.id;
+      this.getTop();
+    }
+  }
+};
+</script>
 
 <style scoped>
 
